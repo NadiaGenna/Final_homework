9244979364bab3d16ca748f9bf2d074cc7012725
«Nadezhda <nadezhdagenna@mail.ru>
2022-09-17 19:29:54 +0300

    Corrections is completely finished

--- Final_Task/Program.cs
+++ Final_Task/Program.cs
@@ -10,7 +10,6 @@
 string[] array1 = { "hello", "2", "world", ":-)" };
 string[] array2 = { "1234", "1567", "-2", "computer science" };
 string[] array3 = { "Russia", "Denmark", "Kazan" };
-
 string[][] matrix = new string[][] { array1, array2, array3 };
 
 for (int i = 0; i < matrix.Length; i++)
@@ -24,7 +23,6 @@ for (int i = 0; i < matrix.Length; i++)
     PrintArray(GetResArray(GetArrayLimitLength(matrix[i]), length_array));
     Console.WriteLine("]");
 }
-
 string[] GetArrayLimitLength(string[] arr)
 {
     string[] res = new string[arr.Length];
